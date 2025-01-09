56d5ff4e0d2b4ad643e348c0b8c656525ba1d1d0
Damir <nimadjonov.2404@gmail.com>
2025-01-09 16:40:44 +0500

    changed env example

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ok2w2+=duf=#47vvw1*l%bvtn0h^ml(v^a&_(peg6wwsn-zs)9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
